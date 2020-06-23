c7f16d444d032f5821e7d9d925cff93232e9d751
Matt Billings <mbillings@gmail.com>
2020-06-22 17:22:55 -0700

    create proper local func

--- lib/crow_io.lua
+++ lib/crow_io.lua
@@ -8,9 +8,50 @@ local NUM_INS = 2
 local NUM_OUTS = 4
 local NUM_TRACKS = 7
 
+local function init_outputs()
+  for track=1,NUM_OUTS do
+    c.output[track].action = "pulse("..GATE_TIME..","..GATE_LEVEL..")"
+  end
+end
+
+local function get_input_param(v)
+  local input_params = {
+    [1] = {},
+    [2] = {"clock_tempo"},
+    [3] = {"swing_amount"},
+    [4] = {"grids_pattern_x"},
+    [5] = {"grids_pattern_y"},
+    [6] = {"grids_pattern_x", "grids_pattern_y"},
+    [7] = {"pattern_chaos"},
+  }
+  return input_params[v]
+end
+
+local function query_input(track)
+  local prev_val = 0
+
+  c.input[track].stream = function(v)
+    if CrowIO:is_crow_in_enabled() then
+      local input_params = get_input_param(params:get("crow_in_"..track.."_param"))
+      local delta = v > prev_val and 2 or -2
+      for param=1, #input_params do
+        params:delta(input_params[param], delta)
+      end
+      prev_val = v
+    end
+  end
+end
+
+local function init_inputs()
+  for track=1,NUM_INS do
+    c.input[track].mode("stream", 0.1)
+    query_input(track)
+  end
+end
+
 function CrowIO:initialize()
-  _init_outputs()
-  _init_inputs()
+  init_outputs()
+  init_inputs()
 end
 
 function CrowIO:add_params()
@@ -27,47 +68,6 @@ function CrowIO:add_params()
       {"Off", "Tempo", "Swing", "Pattern X", "Pattern Y", "Pattern X + Y", "Chaos"},
       1
     )
-  end
-end
-
-function _init_outputs()
-  for track=1,NUM_OUTS do
-    c.output[track].action = "pulse("..GATE_TIME..","..GATE_LEVEL..")"
-  end
-end
-
-function _get_input_param(v)
-  local input_params = {
-    [1] = {},
-    [2] = {"clock_tempo"},
-    [3] = {"swing_amount"},
-    [4] = {"grids_pattern_x"},
-    [5] = {"grids_pattern_y"},
-    [6] = {"grids_pattern_x", "grids_pattern_y"},
-    [7] = {"pattern_chaos"},
-  }
-  return input_params[v]
-end
-
-function _init_inputs()
-  for track=1,NUM_INS do
-    c.input[track].mode("stream", 0.1)
-    _query_input(track)
-  end
-end
-
-function _query_input(track)
-  local prev_val = 0
-
-  c.input[track].stream = function(v)
-    if CrowIO:is_crow_in_enabled() then
-      local input_params = _get_input_param(params:get("crow_in_"..track.."_param"))
-      local delta = v > prev_val and 2 or -2
-      for param=1, #input_params do
-        params:delta(input_params[param], delta)
-      end
-      prev_val = v
-    end
   end
 end
 
