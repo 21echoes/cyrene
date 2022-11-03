88e050ca7fe8b198420f807ff843d748b8b100bb
David Kettler <21echoes@gmail.com>
2022-11-03 13:00:29 -0700

    More swing UI clarity (tuple controls are all in their own section)

--- lib/ui/swing.lua
+++ lib/ui/swing.lua
@@ -9,10 +9,7 @@ local active_hi_level = 15
 local active_lo_level = 6
 local inactive_hi_level = 3
 local inactive_lo_level = 1
-local CLICK_DURATION = 0.7
 local font_size = 16
-local med_font_size = 16
-local small_font_size = 8
 
 local SwingUI = {}
 
@@ -26,12 +23,14 @@ function SwingUI:new()
   local y1 = 14
   local y2 = 49
   local val_title_gap = font_size - 2
-  i.swing_type_title_label = Label.new({x=x1, y=y1, text="SWING", font_size=font_size})
-  i.swing_type_val_label = Label.new({x=x1, y=y1+val_title_gap, font_size=font_size})
-  i.swing_amt_title_label = Label.new({x=x2, y=y1, text="AMNT", font_size=font_size})
-  i.swing_amt_val_label = Label.new({x=x2, y=y1+val_title_gap, font_size=font_size})
-  i.swing_feel_title_label = Label.new({x=x2, y=y2, text="FEEL", font_size=font_size})
-  i.swing_feel_val_label = Label.new({x=x2, y=y2+val_title_gap, font_size=font_size})
+  i.type_title_label = Label.new({x=x1, y=y1, text="SWING", font_size=font_size})
+  i.type_val_label = Label.new({x=x1, y=y1+val_title_gap, font_size=font_size})
+  i.amt_title_label = Label.new({x=x2, y=y1, text="AMNT", font_size=font_size})
+  i.amt_val_label = Label.new({x=x2, y=y1+val_title_gap, font_size=font_size})
+  i.basis_title_label = Label.new({x=x1, y=y2, text="BASIS", font_size=font_size})
+  i.basis_val_label = Label.new({x=x1, y=y2+val_title_gap, font_size=font_size})
+  i.feel_title_label = Label.new({x=x2, y=y2, text="FEEL", font_size=font_size})
+  i.feel_val_label = Label.new({x=x2, y=y2+val_title_gap, font_size=font_size})
 
   i._cached_basis = 2
   i._section = 0
@@ -52,6 +51,10 @@ end
 function SwingUI:add_params_for_track(track, arcify)
 end
 
+function SwingUI:_get_is_simple_swing()
+  return params:get("shuffle_basis") == 1
+end
+
 function SwingUI:enc(n, delta, sequencer)
   if self._section == 0 then
     if n == 2 then
@@ -64,20 +67,21 @@ function SwingUI:enc(n, delta, sequencer)
         params:set("shuffle_basis", self._cached_basis)
       end
     elseif n == 3 then
-      local is_simple_swing = params:get("shuffle_basis") == 1
-      if is_simple_swing then
+      if self:_get_is_simple_swing() then
         params:delta('swing_amount', delta)
-      else
-        params:delta('shuffle_basis', delta)
-        -- Don't let enc 3 go to simple shuffle
-        if params:get("shuffle_basis") == 1 then
-          params:set("shuffle_basis", 2)
-        end
       end
     end
   elseif self._section == 1 then
     if params:get("shuffle_basis") > 1 then
-      params:delta("shuffle_feel", delta)
+      if n == 2 then
+        params:delta('shuffle_basis', delta)
+        -- Don't let section 1 take us back to simple shuffle
+        if params:get("shuffle_basis") == 1 then
+          params:set("shuffle_basis", 2)
+        end
+      elseif n == 3 then
+        params:delta("shuffle_feel", delta)
+      end
     end
   end
   UIState.screen_dirty = true
@@ -87,6 +91,9 @@ function SwingUI:key(n, z, sequencer)
   if (n == 2 and z == 0) or (n == 3 and z == 1) then
     local direction = n == 2 and -1 or 1
     self._section = (self._section + direction + 2) % 2
+    if self:_get_is_simple_swing() then
+      self._section = 0
+    end
     self:_update_active_section()
   end
 end
@@ -108,39 +115,52 @@ local shuffle_feel_names = {
 }
 
 function SwingUI:redraw(sequencer)
-  local is_simple_swing = params:get("shuffle_basis") == 1
-  self.swing_type_title_label:redraw()
-  self.swing_type_val_label.text = is_simple_swing and "PCT" or "TUPLET"
-  self.swing_type_val_label:redraw()
-  self.swing_amt_title_label.text = is_simple_swing and "AMNT" or "BASIS"
-  self.swing_amt_title_label:redraw()
-  if is_simple_swing then
-    self.swing_amt_val_label.text = util.round(params:get("swing_amount"), 1) .. "%"
-    self.swing_amt_val_label.font_size = font_size
-  else
-    self.swing_amt_val_label.text = shuffle_basis_names[params:get("shuffle_basis")]
-    self.swing_amt_val_label.font_size = med_font_size
-  end
-  self.swing_amt_val_label:redraw()
+  local is_simple_swing = self:_get_is_simple_swing()
 
-  self.swing_feel_val_label.text = shuffle_feel_names[params:get("shuffle_feel")]
-  self.swing_feel_title_label.level = self._section == 1 and active_lo_level or inactive_lo_level
-  self.swing_feel_val_label.level = self._section == 1 and active_hi_level or inactive_hi_level
-  if is_simple_swing then
-    self.swing_feel_title_label.level = 0
-    self.swing_feel_val_label.level = 0
+  self.type_val_label.text = is_simple_swing and "PCT" or "TUPLET"
+  self.type_title_label:redraw()
+  self.type_val_label:redraw()
+
+  self.amt_title_label.level = self._section == 0 and active_lo_level or inactive_lo_level
+  self.amt_val_label.level = self._section == 0 and active_hi_level or inactive_hi_level
+  if not is_simple_swing then
+    self.amt_title_label.level = 0
+    self.amt_val_label.level = 0
   end
-  self.swing_feel_title_label:redraw()
-  self.swing_feel_val_label:redraw()
+  self.amt_val_label.text = util.round(params:get("swing_amount"), 1) .. "%"
+  self.amt_title_label:redraw()
+  self.amt_val_label:redraw()
+
+  self.basis_title_label.level = self._section == 1 and active_lo_level or inactive_lo_level
+  self.basis_val_label.level = self._section == 1 and active_hi_level or inactive_hi_level
+  if is_simple_swing then
+    self.basis_title_label.level = 0
+    self.basis_val_label.level = 0
+  end
+  self.basis_val_label.text = shuffle_basis_names[params:get("shuffle_basis")]
+  self.basis_title_label:redraw()
+  self.basis_val_label:redraw()
+
+  self.feel_title_label.level = self._section == 1 and active_lo_level or inactive_lo_level
+  self.feel_val_label.level = self._section == 1 and active_hi_level or inactive_hi_level
+  if is_simple_swing then
+    self.feel_title_label.level = 0
+    self.feel_val_label.level = 0
+  end
+  self.feel_val_label.text = shuffle_feel_names[params:get("shuffle_feel")]
+  self.feel_title_label:redraw()
+  self.feel_val_label:redraw()
 end
 
 function SwingUI:_update_active_section()
-  self.swing_type_title_label.level = self._section == 0 and active_lo_level or inactive_lo_level
-  self.swing_type_val_label.level = self._section == 0 and active_hi_level or inactive_hi_level
-  self.swing_amt_title_label.level = self._section == 0 and active_lo_level or inactive_lo_level
-  self.swing_amt_val_label.level = self._section == 0 and active_hi_level or inactive_hi_level
-  self.swing_feel_title_label.level = self._section == 1 and active_lo_level or inactive_lo_level
-  self.swing_feel_val_label.level = self._section == 1 and active_hi_level or inactive_hi_level
+  self.type_title_label.level = self._section == 0 and active_lo_level or inactive_lo_level
+  self.type_val_label.level = self._section == 0 and active_hi_level or inactive_hi_level
+  self.amt_title_label.level = self._section == 0 and active_lo_level or inactive_lo_level
+  self.amt_val_label.level = self._section == 0 and active_hi_level or inactive_hi_level
+  self.basis_title_label.level = self._section == 1 and active_lo_level or inactive_lo_level
+  self.basis_val_label.level = self._section == 1 and active_hi_level or inactive_hi_level
+  self.feel_title_label.level = self._section == 1 and active_lo_level or inactive_lo_level
+  self.feel_val_label.level = self._section == 1 and active_hi_level or inactive_hi_level
   UIState.screen_dirty = true
 end
 
